c38ded9094d8c4b990c085077c88453faa4187f6
Nick Healy <nickrhealy@smarkets.com>
2021-12-27 19:16:49 -0800

    update port to use port in node env

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const { initCronJob } = require('./crawler_scheduler')
 
 const app = express()
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.static('public'))
   
 app.listen(PORT, () => {
     initCronJob()
-    console.log(`Listening at http://localhost:${PORT}`)
+    console.log(`Listening at port ${PORT}`)
 })
