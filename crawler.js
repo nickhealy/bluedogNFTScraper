304d2103de7041fe73aeac41712bc21e98af5dbc
Nick Healy <nickhealy@Nicks-MacBook-Pro.local>
2022-01-03 14:30:58 +0000

    get description info from ghost market, add helper functions for crawler

--- crawler.js
+++ crawler.js
@@ -1,36 +1,42 @@
 const puppeteer = require('puppeteer');
-const fs = require('fs')
-const path = require('path');
 const { uploadData } = require('./s3');
 
-const text = el => el.innerHTML;
+const toText = el => el.innerHTML;
 
-const GHOST_URL = 'https://ghostmarket.io/account/pha/P2K83Mn8suXudonzCgv5K3GgXkhToeQUgHZh2uArFpmav4a/?tab=onsale'
+const GHOST_MARKET_BASE = 'https://ghostmarket.io/'
+
+const GHOST_URL = `${GHOST_MARKET_BASE}account/pha/P2K83Mn8suXudonzCgv5K3GgXkhToeQUgHZh2uArFpmav4a/?tab=onsale`
 const CONNECTION_ATTEMPTS = 5
 
-const ensurePage = async page => {
+const ensurePage = async (page, url) => {
   try {
-    await page.goto(GHOST_URL, { waitUntil: 'networkidle0' });
-    await page.waitForSelector('.asset-card', {
-      timeout: 4000
-    });
+    await page.goto(url, { waitUntil: 'networkidle0' });
+    return true;
   } catch (e) {
     console.error(e)
     return false
   }
 }
 
-const getPageData = async page => {
+const getPageData = async (page, url) => {
   let hasData = false;
   let attempts = 0;
-  while (attempts < CONNECTION_ATTEMPTS && hasData === false) {
-    hasData = await ensurePage(page)
+  while (attempts < CONNECTION_ATTEMPTS && !hasData) {
+    hasData = await ensurePage(page, url)
     attempts++
-    if (hasData === false) {
+    if (!hasData) {
        // Wait a few seconds, also a good idea to swap proxy here
       await new Promise((resolve) => setTimeout(resolve, 3000));
     }
   }
+}
+
+const getContainingElement = async (page, selector, timeout = 4000) => {
+  await page.waitForXPath(selector, {
+    timeout
+  });
+  const result = await page.$x(selector)
+  return result[0] // page.$x returns array, but this function is meant to return a single element
 }
 
 const getNftData = async () => {
@@ -40,23 +46,24 @@ const getNftData = async () => {
     args: ['--no-sandbox',]});
 
   const page = await browser.newPage();
-  await getPageData(page);
+  await getPageData(page, GHOST_URL);
 
-  const nftData = await page.$$('.asset-card')
-
-
+  const nftDataContainer = await getContainingElement(page, '//*[@id="onsale-content"]/div/div/div[2]/div/div[1]')
+  const nftCards = await nftDataContainer.$$('.asset-card')
   const parsedData = [];
-  for (const card of nftData) {
+  for (const card of nftCards) {
     const isOnSale = await card.$eval('.crypto', saleInfo => saleInfo.innerHTML !== ' not on sale')
     if (isOnSale) {
       const saleLink = await card.$eval('a', aTag => aTag.getAttribute('href'))
-      const name = await card.$eval('.name.h-full.ellipsis', text);
+      const name = await card.$eval('.name.h-full.ellipsis', toText);
       const imgSrc = await card.$eval('#image-thumb-layer', img => img.getAttribute('src'))
-      const priceCrypto = await card.$eval('.crypto', text)
-      const priceUSD = await card.$eval('.fiat.subtext', text)
+      const priceCrypto = await card.$eval('.crypto', toText)
+      const priceUSD = await card.$eval('.fiat.subtext', toText)
+
+      const description = await getNftDescription(saleLink, browser)
 
       parsedData.push({
-        saleLink, name, imgSrc, priceCrypto, priceUSD
+        saleLink, name, imgSrc, priceCrypto, priceUSD, description
       })
     }
   }
@@ -64,6 +71,15 @@ const getNftData = async () => {
 
   await browser.close();
 };
+
+const getNftDescription = async (href, browser) => {
+  const finalUrl = `${GHOST_MARKET_BASE}${href}`
+  const page = await browser.newPage()
+  await getPageData(page, finalUrl)
+  const description = await getContainingElement(page, '//*[@id="__layout"]/div/section/div/div[2]/div[1]/div/div[1]')
+  const text = await description.$eval('.text.ellipsis', toText)
+  return text.split('\n')[0] // we only want text from blockchain, which is the first element
+}
 
 getNftData()
   .then("Finished crawling!")
